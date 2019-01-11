b3df31597f868aa764ddbb01314c1a3d0a27a4f2
GFabien <fabien.girka@supelec.fr>
2019-01-11 22:15:20 +0100

    Added cross image on delete button and associated style

--- main.js
+++ main.js
@@ -1,79 +1,75 @@
-Vue.component('todo', {
-    props: {
-        name: {
-            type: String,
-            required: true
-        },
-        index: {
-            type: Number,
-            required: true
-        }
+Vue.component("todo", {
+  props: {
+    name: {
+      type: String,
+      required: true
     },
-    template: `
-    <div>
+    index: {
+      type: Number,
+      required: true
+    }
+  },
+  template: `
+    <div class="todo">
+        <button v-on:click="deleteTodo"><img :src="image" alt="cross"/></button>
         <p>{{ name }}</p>
-        <input type="checkbox" v-model="done">
-        <button v-on:click="deleteTodo">Delete todo</button>
     </div>
     `,
-    data() {
-        return {
-            done: false
-        }
-    },
-    methods: {
-        deleteTodo() {
-            this.$emit('todo-deleted', this.index);
-        }
-    },
-    computed: {
-
+  data() {
+    return {
+      done: false,
+      image: "./cross.png"
+    };
+  },
+  methods: {
+    deleteTodo() {
+      this.$emit("todo-deleted", this.index);
     }
-})
+  },
+  computed: {}
+});
 
-Vue.component('add-todo', {
-    template: `
+Vue.component("add-todo", {
+  template: `
     <form @submit.prevent="onSubmit">
         <p>
             <label for="newTodo">New todo:</label>
             <input id="newTodo" v-model="newTodo" required>
         </p>
         <p>
-          <input type="submit" value="Submit">  
+          <input type="submit" value="Add todo">  
         </p>
     </form>
     `,
-    data() {
-        return {
-            newTodo: null
-        }
-    },
-    methods: {
-        onSubmit() {
-            let todo = {
-                name: this.newTodo,
-                done: false
-            }
-            this.$emit('add-todo-submitted', todo);
-            this.name = null;
-        }
-    },
-    computed: {
-
+  data() {
+    return {
+      newTodo: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      let todo = {
+        name: this.newTodo,
+        done: false
+      };
+      this.$emit("add-todo-submitted", todo);
+      this.newTodo = null;
     }
-})
+  },
+  computed: {}
+});
 
 const app = new Vue({
-    el: '#app',
-    data: {
-        todoList: []
+  el: "#app",
+  data: {
+    todoList: []
+  },
+  methods: {
+    addTodo(todo) {
+      this.todoList.push(todo);
     },
-    methods: {
-        addTodo(todo) {
-            this.todoList.push(todo);
-        },
-        deleteTodo(index) {
-            this.todoList.splice(index, 1);
-        }
+    deleteTodo(index) {
+      this.todoList.splice(index, 1);
     }
-})
+  }
+});
