0a2ee05a5fae48f3e6aeb511af7db4fcc33c00ac
GFabien <fabien.girka@supelec.fr>
2019-01-11 18:58:37 +0100

    Added component add-todo

--- main.js
+++ main.js
@@ -25,6 +25,31 @@ Vue.component('todo', {
     }
 })
 
+Vue.component('add-todo', {
+    template: `
+    <form>
+        <p>
+            <label for="newTodo">New todo:</label>
+            <input id="newTodo" v-model="newTodo" required>
+        </p>
+        <p>
+          <input type="submit" value="Submit">  
+        </p>
+    </form>
+    `,
+    data() {
+        return {
+            newTodo: null
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+})
+
 const app = new Vue({
     el: '#app',
     data: {
